25f0c62d072c43a09b05974cdf94fd5b8a381562
Mika Senghaas <mail@mikasenghaas.de>
2023-11-11 00:06:52 +0100

    feat(*): renamed utils into src

--- src/embedders.py
+++ src/embedders.py
@@ -1,11 +1,11 @@
 from abc import abstractmethod
 import numpy as np
 
-class EmbeddorBase:
 
+class EmbeddorBase:
     def __init__(self) -> None:
         pass
 
     @abstractmethod
-    def transform(self, reviews : [str]) -> np.ndarray:
+    def transform(self, reviews: [str]) -> np.ndarray:
         pass
---
+++ src/extractors.py
@@ -0,0 +1,36 @@
+"""This module contains the ExtractorBase class and its subclasses.
+These classes are used to extract features from a list of spaCy documents
+corresponding to the beer reviews.
+"""
+
+
+from abc import abstractmethod
+from spacy import Doc
+
+
+class ExtractorBase:
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def transform(self, docs: [Doc]) -> [str]:
+        pass
+
+
+class LemmaExtractor(ExtractorBase):
+    def __init__(self):
+        super().__init__()
+
+    def transform(self, docs: [Doc]) -> [str]:
+        return [" ".join([token.lemma_ for token in doc]) for doc in docs]
+
+
+class AdjectiveExtractor(ExtractorBase):
+    def __init__(self):
+        super().__init__()
+
+    def transform(self, docs: [Doc]) -> [str]:
+        return [
+            " ".join([token.text for token in doc if token.pos_.startswith("ADJ")])
+            for doc in docs
+        ]
