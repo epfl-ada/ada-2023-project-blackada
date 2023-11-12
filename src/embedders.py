9a86768f861af10f7d829594e960a2d0e0246351
Pierre Lardet <lardet.pierre@gmail.com>
2023-11-12 21:27:46 +0100

    feat: add torch device utility function

--- src/embedders.py
+++ src/embedders.py
@@ -7,5 +7,16 @@ class EmbeddorBase:
         pass
 
     @abstractmethod
-    def transform(self, reviews: [str]) -> np.ndarray:
-        pass
+def _get_torch_device() -> torch.device:
+    """
+    Returns the device to be used for PyTorch operations.
+
+    Returns:
+        torch.device: Device to be used for PyTorch operations.
+    """
+    if torch.backends.mps.is_available():
+        return torch.device("mps")
+    elif torch.cuda.is_available():
+        return torch.device("cuda")
+    else:
+        return torch.device("cpu")
