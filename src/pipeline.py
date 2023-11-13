eeb51a3fe133ebf28cd6a1a3f6fb680714807264
peterstran <nuttepet@fjfi.cvut.cz>
2023-11-13 20:45:57 +0100

    Add docstrings to pipeline

---
+++ src/pipeline.py
@@ -0,0 +1,51 @@
+"""
+Text Analysis Workflow Module
+
+This module introduces the TextAnalysis class, which provides a streamlined workflow for 
+processing text documents. It integrates feature extraction, embedding, and metric computation 
+in a cohesive manner. The class is designed to be flexible, accepting any implementations 
+of extractor, embeddor, and metric calculator that conform to a standard interface.
+"""
+
+import numpy as np
+from typing import List, Any
+
+
+class TextAnalysis:
+    """
+    A class that integrates text extraction, embedding, and metric computation.
+
+    This class is designed to work with any extractor, embeddor, and metric
+    computation method, as long as they adhere to a standard interface.
+    """
+
+    def __init__(self, extractor: Any, embeddor: Any, metric: Any) -> None:
+        """
+        Initializes the TextAnalysis with specified extractor, embeddor, and metric.
+
+        Parameters:
+        extractor (Any): An object that implements a transform method for text extraction.
+        embeddor (Any): An object that implements a transform method for embedding the extracted text.
+        metric (Any): An object that implements a transform method for computing a metric on the embeddings.
+        """
+        self.extractor = extractor
+        self.embeddor = embeddor
+        self.metric = metric
+
+    def transform(self, docs: List[str]) -> np.ndarray:
+        """
+        Processes the given documents through extraction, embedding, and metric computation.
+
+        Parameters:
+        docs (List[str]): A list of documents to be processed.
+
+        Returns:
+        np.ndarray: The resulting similarity scores or metrics after processing the documents.
+        """
+        # Extraction
+        extracted_texts = self.extractor.transform(docs)
+        # Embedding
+        embeddings = self.embeddor.transform(extracted_texts)
+        # Similarity Calculation
+        self.similarity = self.metric.transform(embeddings)
+        return self.similarity
